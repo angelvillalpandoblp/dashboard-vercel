6019c3dbec67ad66c21306488944899a68108ef7
angelvillalpandoblp <angel.villalpandogpooabx@gmail.com>
2026-03-09 10:07:40 -0600

    +1+1

--- script.js
+++ script.js
@@ -1194,7 +1194,7 @@ function ejecutarExplosion() {
                     
                     const inicioAño = new Date(hoy.getFullYear(), 0, 1);
                     const dias = Math.floor((hoy - inicioAño) / (24 * 60 * 60 * 1000));
-                    const semanaStr = "Semana " + Math.ceil((hoy.getDay() + 1 + dias) / 7);
+                    const semanaStr = "Semana " + Math.ceil((hoy.getDay() + 1 + dias) / 7) + 1;
 
                     // Llama a la función que conecta con tu Mini-API del archivo Destino
                     enviarDatos(mesMayuscula, semanaStr, sumaTotal, btn, res);
