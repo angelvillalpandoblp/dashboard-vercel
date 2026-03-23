1ae5f52f68df837cea636a7c56a66fca29802c1c
angelvillalpandoblp <angel.villalpandogpooabx@gmail.com>
2026-03-23 15:38:05 -0600

    semana 13

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function openGavetaTable(gid, title) {
     if (!modal) return;
 
     modal.style.display = 'flex';
-    modalTitle.textContent = title + " . Semana 11";
+    modalTitle.textContent = title + " . Semana 13";
     header.innerHTML = "";
     body.innerHTML = "<tr><td colspan='10' style='text-align:center; padding:40px;'>Cargando...</td></tr>";
 
